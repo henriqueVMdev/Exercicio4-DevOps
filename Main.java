c177850c3b30153efb12bf71dd9c29aa152a4584
henriqueVMdev <henrique.de.paula.valim@gmail.com>
2026-09-05 16:31:31 -0300

    Codigo exercicio 5 Aula 4

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+
+    Pet atendimento1 = new Pet("Jailson", "cachorro", 10, 2990.9);
+
+    atendimento1.exibirInformacoes();
+    }
+}
---
+++ Pet.java
@@ -0,0 +1,17 @@
+public class Pet {
+    String nome;
+    String especie;
+    int idade;
+    double valorServico;
+
+    public Pet(String nomePet, String especiePet, int idadePet, double  valorServicoPet){
+        nome = nomePet;
+        especie = especiePet;
+        idade = idadePet;
+        valorServico = valorServicoPet;
+    }
+
+    void exibirInformacoes(){
+        System.out.println("Nome: "+nome+"\nEspecie: "+ especie+"\nIdade: "+idade+"\nValor do servico: "+ valorServico);
+    }
+}
